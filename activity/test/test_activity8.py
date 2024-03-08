0ea8ffa27fefced50ea4bd1d41ee52290f0c0ab6
KartikKARTIK-4498 <kartik.khangwal1@gmail.com>
2024-03-08 14:34:11 +0530

    Activity-8

---
+++ activity/test/test_activity8.py
@@ -0,0 +1,13 @@
+from activity.activity8 import guess_the_number
+from unittest.mock import patch
+
+def test_guess_the_number_incorrect_guess():
+    with patch('builtins.input', side_effect=['50', '75', '88', '92', '95']):
+        result = guess_the_number()
+    assert result is False
+
+
+def test_guess_the_number_out_of_attempts():
+    with patch('builtins.input', side_effect=['10', '20', '30', '40', '50']):
+        result = guess_the_number()
+    assert result is False
