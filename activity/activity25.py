a47469421664646c6438d4552de4d0cbd6191c66
KartikKARTIK-4498 <kartik.khangwal1@gmail.com>
2024-03-08 15:10:34 +0530

    Activity-25

---
+++ activity/activity25.py
@@ -0,0 +1,11 @@
+# Prerequisites
+# 1. Installing Python (3.6 or higher) Resources Needed
+# 2. EasyOCR library for text recognition.
+# 3. OpenCV library for image preprocessing.
+# 4. Tkinter for creating the graphical user interface.
+# 5. JSON library for structured data manipulation.
+# 6. JSON file containing puzzles and predefined answers.
+
+
+
+# all installed as per the requirements 
